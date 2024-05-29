77d0a5148a350804d84645e757713602f0ca919b
Silous <silous666@gmail.com>
2024-05-29 22:53:18 +0200

    add call to repack the game

---
+++ ze_ztd_tool.py
@@ -0,0 +1,11 @@
+"""call the repack process"""
+
+import subprocess
+
+
+TOOL_ZTD: str = "ZTD_patch_data\\ze_ztd_repack.bat"
+
+
+def repack_ztd():
+    """repack the game"""
+    subprocess.run(TOOL_ZTD, shell=True, creationflags=subprocess.CREATE_NO_WINDOW, check=False)
